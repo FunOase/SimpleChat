013984aafdcf932e5ff798692da4af3f72d87b5b
RappyTV <rappytv@rappytv.com>
2024-02-11 04:17:29 +0100

    Adapt /emoji to RyLib

--- src/main/java/com/rappytv/chat/commands/Emoji.java
+++ src/main/java/com/rappytv/chat/commands/Emoji.java
@@ -1,32 +1,45 @@
 package com.rappytv.chat.commands;
 
 import com.rappytv.chat.ChatPlugin;
+import com.rappytv.rylib.RyLib;
+import com.rappytv.rylib.util.Command;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@SuppressWarnings("all")
-public class Emoji implements CommandExecutor {
+import java.util.List;
+
+public class Emoji extends Command<ChatPlugin> {
+
+    public Emoji(String name, ChatPlugin plugin) {
+        super(name, plugin);
+    }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String prefix, String[] args) {
-        if(!(sender instanceof Player)) {
-            sender.sendMessage(ChatPlugin.prefix + "§cDie Konsole darf diesen Befehl nicht ausführen!");
-            return false;
+    public void execute(CommandSender sender, String prefix, String[] args) {
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(RyLib.get().i18n().translate("onlyPlayer"));
+            return;
         }
-        Player player = (Player) sender;
 
         if(!player.hasPermission("chat.emojis.toggle")) {
-            player.sendMessage(ChatPlugin.prefix + "§cDazu hast du keine Rechte!");
-            return false;
+            player.sendMessage(RyLib.get().i18n().translate("noPermission"));
+            return;
         }
         boolean active = !player.hasPermission("chat.emojis");
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set chat.emojis " + active);
 
-        String text = active ? "§aaktiviert" : "§cdeaktiviert";
-        player.sendMessage(ChatPlugin.prefix + "Emojis sind nun " + text + "§7!");
-        return true;
+        String text = active
+                ? plugin.i18n().translate("command.emoji.activated")
+                : plugin.i18n().translate("command.emoji.deactivated");
+        player.sendMessage(
+                plugin.i18n().translate("command.emoji.success")
+                        .replace("<state>", text)
+        );
+    }
+
+    @Override
+    public List<String> complete(CommandSender sender, String prefix, String[] args) {
+        return null;
     }
 }
