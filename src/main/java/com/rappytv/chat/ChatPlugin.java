adc92a7d38e27ac5b5c6e0c8d54c9bc025e13eab
RappyTV <rappytv@rappytv.com>
2024-02-11 04:18:20 +0100

    Adapt main file to RyLib

--- src/main/java/com/rappytv/chat/ChatPlugin.java
+++ src/main/java/com/rappytv/chat/ChatPlugin.java
@@ -7,30 +7,31 @@ import com.rappytv.chat.events.JoinListener;
 import com.rappytv.chat.events.PlayerChatListener;
 import com.rappytv.chat.events.luckperms.UpdateListener;
 import com.rappytv.chat.util.LuckPermsUtil;
+import com.rappytv.rylib.util.I18n;
+import com.rappytv.rylib.util.UpdateChecker;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
-
 public final class ChatPlugin extends JavaPlugin {
 
-    public static String prefix;
     public LuckPerms lp;
+    private I18n i18n;
     private LuckPermsUtil luckPermsUtil;
 
     @Override
     public void onEnable() {
         registerAll();
         saveDefaultConfig();
-
-        prefix = getConfig().contains("prefix")
-                ? ChatColor.translateAlternateColorCodes('&', getConfig().getString("prefix"))
-                : "§e§lChat §8» §7";
+        i18n = new I18n(this);
+        new UpdateChecker<>(this).setArtifactFormat(
+                "ci.rappytv.com",
+                getName(),
+                "com.rappytv"
+        );
 
         LuckPerms provider = Bukkit.getServicesManager().load(LuckPerms.class);
         if (provider != null) {
@@ -45,17 +46,18 @@ public final class ChatPlugin extends JavaPlugin {
         }
     }
 
-    @Override
-    public void onDisable() {}
-
     private void registerAll() {
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new JoinListener(this), this);
         pm.registerEvents(new PlayerChatListener(this), this);
 
-        Objects.requireNonNull(Bukkit.getPluginCommand("chat")).setExecutor(new Chat());
-        Objects.requireNonNull(Bukkit.getPluginCommand("chatclear")).setExecutor(new ChatClear());
-        Objects.requireNonNull(Bukkit.getPluginCommand("emoji")).setExecutor(new Emoji());
+        new Chat("chat", this);
+        new ChatClear("chatclear", this);
+        new Emoji("emoji", this);
+    }
+
+    public I18n i18n() {
+        return i18n;
     }
 
     public LuckPermsUtil getLuckPermsUtil() {
