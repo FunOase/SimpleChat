ed0eec447329b599ec51eff304c6dd9e601cf0ec
RappyTV <rappytv@rappytv.com>
2024-03-02 15:01:22 +0100

    Check if scoreboards are enabled, move #setTabPrefix down because of buggy behaviour

--- src/main/java/com/rappytv/chat/events/JoinListener.java
+++ src/main/java/com/rappytv/chat/events/JoinListener.java
@@ -20,8 +20,8 @@ public class JoinListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        if(plugin.getConfig().getBoolean("i18n.tab.enabled")) new TablistScoreboard(player);
+        if(plugin.getConfig().getBoolean("i18n.scoreboard.enabled")) new SidebarScoreboard(player);
         plugin.getLuckPermsUtil().setTabPrefix(player);
-        new TablistScoreboard(player);
-        new SidebarScoreboard(player);
     }
 }
